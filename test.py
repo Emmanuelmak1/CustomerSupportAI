4f0b30aea72d25c157c3885b747990c0afe352e9
Emmanuelmak1 <makinde.oluwagbogo@gmail.com>
2024-02-27 15:48:41 -0800

    Summary

---
+++ test.py
@@ -0,0 +1,9 @@
+# Import the generate_comment function from your Flask application script
+from app import generate_comment
+
+# Call the generate_comment function
+comment = generate_comment()
+
+# Print the generated comment
+print("Generated comment:")
+print(comment)
